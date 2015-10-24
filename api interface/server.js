11bc4b6ab10ee6b79c69476196ec4f640a84b4d6
Flanders Lorton <florton@lion.lmu.edu>
2015-10-24 16:18:17 -0700

    web app now takes user input!

--- api interface/server.js
+++ api interface/server.js
@@ -4,10 +4,11 @@ var request = require('request');
 var app = express();  
 function proxy(){
   app.use('/', function(req, res) {
+    var sid = req.query.sid;
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    var apiServerHost = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamids=76561197972495328";
-    var url = apiServerHost;
+    var qs  = "?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamids=" + sid;
+    var url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/" + qs; 
     req.pipe(request(url)).pipe(res);
   });
 }

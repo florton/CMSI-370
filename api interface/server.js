2a4eb15e9d3c4e548a7b917610d8cf7117c7bfb7
Flanders Lorton <florton@lion.lmu.edu>
2015-10-24 18:22:26 -0700

    Now accepts both numerical ids and vanity ids !
    this was a pain, had to do a second api call and avoided having to deal
    with async

--- api interface/server.js
+++ api interface/server.js
@@ -4,11 +4,21 @@ var request = require('request');
 var app = express();  
 function proxy(){
   app.use('/', function(req, res) {
-    var sid = req.query.sid;   
+    var sid = req.query.sid[1].trim();
+    var url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamids="    
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
-    var url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamids="+ sid;    
-    req.pipe(request(url)).pipe(res);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
+    
+    if (!/[\d]{17}/.test(sid)) {
+        request("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&&vanityurl="+sid
+        , function (error, response, body){
+           var newid = JSON.parse(body).response.steamid
+           req.pipe(request(url+newid)).pipe(res);
+        });
+    }else{
+        req.pipe(request(url+sid)).pipe(res);
+    }  
+
   });
 }
 

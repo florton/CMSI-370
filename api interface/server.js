f9d64cf630211abbd762918150550b5398503d46
Flanders Lorton <florton@lion.lmu.edu>
2015-10-25 01:30:02 -0700

    Formatted all available data from user summary

--- api interface/server.js
+++ api interface/server.js
@@ -1,31 +1,28 @@
-var express = require('express');  
+var express = require('express');
 var request = require('request');
+var app = express();
 
-var app = express();  
-function proxy(){
-  app.use('/', function(req, res) {
-    var sid = req.query.sid[1].trim();
-    var url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamids="    
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
-    
-    if (!/[\d]{17}/.test(sid)) {
-        request("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&&vanityurl="+sid
-        , function (error, response, body){
-           var newid = JSON.parse(body).response.steamid
-           req.pipe(request(url+newid)).pipe(res);
+function proxy() {
+    app.use('/', function(req, res) {
+        var sid = req.query.sid[1].trim();
+        var url =
+            "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamids=";
+        var url2 =
+            "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&vanityurl=";
+        //var url3 =  "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamid=" + sid + "&format=json";
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept");
+        request(url2 + sid, function(error, response, body) {
+            var newid = JSON.parse(body).response.steamid
+            if (newid !== undefined) {
+                sid = newid;
+            }
+            //request(url+sid, function(err, resp, result){
+            req.pipe(request(url + sid)).pipe(res);
+            //})
         });
-    }else{
-        req.pipe(request(url+sid)).pipe(res);
-    }  
-
-  });
+    });
 }
-
-try{
-    proxy();
-}catch(exception){
-    proxy();
-}
-
-app.listen(process.env.PORT || 3000); 
+proxy();
+app.listen(process.env.PORT || 3000);

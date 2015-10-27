d310a3e5f8b8db786c9653bdbcaf02ea9a992532
Flanders Lorton <florton@lion.lmu.edu>
2015-10-27 10:28:33 -0700

    Now allows for multiple api calls with a flag added to sid

--- api interface/server.js
+++ api interface/server.js
@@ -5,9 +5,13 @@ var app = express();
 function proxy() {
     app.use('/', function(req, res) {
         var sid = req.query.sid[1].trim();
-        var url ="http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamids=";
+        var flag = sid.slice(-1);
+        sid = sid.slice(0, sid.length-1);
+        var url1 ="http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamids=";
         var url2 ="http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&vanityurl=";
-        var url3 =  "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamid=";
+        var url3 = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamid=";
+        var url = ""
+        
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
         request(url2 + sid, function(error, response, body) {
@@ -15,10 +19,8 @@ function proxy() {
             if (newid !== undefined) {
                 sid = newid;
             }
-            //request(url3+sid+"&format=json", function(err, resp, result){
-            //   var second_json = result;
-               req.pipe(request(url + sid)).pipe(res);
-            //})
+            if (flag==1){url = url1+sid;}else{url = url3+sid;}
+            req.pipe(request(url)).pipe(res);
         });
     });
 }

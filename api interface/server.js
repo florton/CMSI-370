043bb5c41565bac3a0e2967adb3b5ab3285afb0d
Flanders Lorton <florton@lion.lmu.edu>
2015-10-27 09:46:19 -0700

    small fixes with time
    commiting before modifying code

--- api interface/server.js
+++ api interface/server.js
@@ -5,21 +5,19 @@ var app = express();
 function proxy() {
     app.use('/', function(req, res) {
         var sid = req.query.sid[1].trim();
-        var url =
-            "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamids=";
-        var url2 =
-            "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&vanityurl=";
-        //var url3 =  "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamid=" + sid + "&format=json";
+        var url ="http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamids=";
+        var url2 ="http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&vanityurl=";
+        var url3 =  "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=FADC45FD3C92FDFFDBCDA01F2A4149A9&steamid=";
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
         request(url2 + sid, function(error, response, body) {
             var newid = JSON.parse(body).response.steamid
             if (newid !== undefined) {
                 sid = newid;
             }
-            //request(url+sid, function(err, resp, result){
-            req.pipe(request(url + sid)).pipe(res);
+            //request(url3+sid+"&format=json", function(err, resp, result){
+            //   var second_json = result;
+               req.pipe(request(url + sid)).pipe(res);
             //})
         });
     });

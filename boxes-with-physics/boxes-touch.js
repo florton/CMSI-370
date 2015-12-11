cac6f95c90684ebec3ff369ea90449ff612a484f
Flanders Lorton <florton@lion.lmu.edu>
2015-12-10 23:44:18 -0800

    flick and gravity work!!!!!!

--- boxes-with-physics/boxes-touch.js
+++ boxes-with-physics/boxes-touch.js
@@ -19,8 +19,8 @@
                 if (touch.target.topPos >= windowHeight-20 ){ touch.target.topPos = windowHeight-20;}
                 if (touch.target.topPos < 0){touch.target.topPos = 0;}
                 
-                document.getElementById("console").innerHTML = "";
-                document.getElementById("console").appendChild(document.createTextNode( "("+touch.target.leftPos+", "+touch.target.topPos+") "+"("+windowWidth+", "+windowHeight+")"));
+                //document.getElementById("console").innerHTML = "";
+                //document.getElementById("console").appendChild(document.createTextNode( "("+touch.target.leftPos+", "+touch.target.topPos+") "+"("+windowWidth+", "+windowHeight+")"));
                 
                 touch.target.movingBox.offset({ 
                     left: touch.target.leftPos,
@@ -41,67 +41,99 @@
             if (touch.target.movingBox) {
                 // Change state to "not-moving-anything" by clearing out
                 // touch.target.movingBox.
-                touch.target.movingBox.flick = true;
                 
+                touch.target.movingBox.flick = true;
                 touch.target.movingBox.xVelocity=touch.target.leftPos-touch.target.leftPosOld;
                 touch.target.movingBox.yVelocity=touch.target.topPos-touch.target.topPosOld;
                 
-                oldTimestamp = 0;
-                window.requestAnimationFrame(flickBoxes);
             }
         });
     };
     
     
-    
-    var flickBoxes = function(timestamp){
-        $("div.box").each(function (index) {
+    var moveBoxes = function(box){
+        if(box.context.movingBox !== undefined){
+            var target = box.context.movingBox;
             
-            var delta = timestamp - oldTimestamp;  
+            //document.getElementById("console").innerHTML = "";
+            //document.getElementById("console").appendChild(document.createTextNode( "("+target.gravityX+", "+ target.gravityY + ")"));
+            
+            target.offset({
+                left:target.offset().left + target.xVelocity - target.gravityX,
+                top:target.offset().top + target.yVelocity + target.gravityY
+            });
+        }
+    }
+   
+   
+    var animateBoxes = function(timestamp){
+        var delta = timestamp - oldTimestamp;  
 
-            if($(this).context.movingBox !== undefined && $(this).context.movingBox.flick ==true){
-                var target = $(this).context.movingBox;
+        if(delta>10){                     
+            $("div.box").each(function (index) { 
+                flickBoxes($(this));
+                moveBoxes($(this))
+            });
+        }
 
+        oldTimestamp = timestamp;
+
+        window.requestAnimationFrame(animateBoxes);
+    }
+    
+    window.ondevicemotion = function(event) {
+        $("div.box").each(function (index) { 
+            if($(this).context.movingBox !== undefined){
+                var target=$(this).context.movingBox;
+                //document.getElementById("console").innerHTML = "";
+                //document.getElementById("console").appendChild(document.createTextNode( "("+target.offset().left+", "+ target.offset().top + ")"));
                 
-                //console.log(delta);
-                if(delta>10){                     
-                    
-
-                    if(target.xVelocity !== 0){
-                        target.xVelocity = (target.xVelocity>0) ? target.xVelocity-1 : target.xVelocity+1;
-                    }
-                    if(target.yVelocity !== 0){
-                        target.yVelocity = (target.yVelocity>0) ? target.yVelocity-1 : target.yVelocity+1;
-                    }
-                    if(target.xVelocity === 0 && target.yVelocity === 0){
-                        target.flick = false;
-                    }
-
-                    
-                    if (target.offset().left >= windowWidth-20){
-                        target.xVelocity = Math.abs(target.xVelocity)*-1;
-                    }else if (target.offset().left < 0){
-                        target.xVelocity = Math.abs(target.xVelocity);
-                    }if (target.offset().top >= windowHeight-20){
-                        target.yVelocity = Math.abs(target.yVelocity)*-1;
-                    }else if (target.offset().top < 0){ 
-                        target.yVelocity = Math.abs(target.yVelocity);
-                    }
-                    
-                    
-                    target.offset({
-                        left:target.offset().left + target.xVelocity,
-                        top:target.offset().top + target.yVelocity
-                    });
-                    
-         
+                if (target.offset().left < windowWidth-20 && event.accelerationIncludingGravity.x < 0 ){ 
+                    target.gravityX = event.accelerationIncludingGravity.x;
+                }else if(target.offset().left > 0 && event.accelerationIncludingGravity.x > 0){
+                    target.gravityX = event.accelerationIncludingGravity.x;
+                }else{
+                    target.gravityX = 0;
+                }
+                if (target.offset().top < windowHeight-20 && event.accelerationIncludingGravity.y > 0 ){ 
+                    target.gravityY = event.accelerationIncludingGravity.y;
+                }else if(target.offset().top > 0 && event.accelerationIncludingGravity.y < 0){
+                    target.gravityY = event.accelerationIncludingGravity.y;
+                }else{
+                    target.gravityY = 0;
                 }
                 
-                oldTimestamp = timestamp;
+
             }
         });
-        
-        window.requestAnimationFrame(flickBoxes);
+  
+    }
+    
+    var flickBoxes = function(box){
+        if(box.context.movingBox !== undefined && box.context.movingBox.flick ==true){
+            var target = box.context.movingBox;
+
+            if(target.xVelocity !== 0){
+                target.xVelocity = (target.xVelocity>0) ? target.xVelocity-1 : target.xVelocity+1;
+            }
+            if(target.yVelocity !== 0){
+                target.yVelocity = (target.yVelocity>0) ? target.yVelocity-1 : target.yVelocity+1;
+            }
+            if(target.xVelocity === 0 && target.yVelocity === 0){
+                target.flick = false;
+            }
+
+            if (target.offset().left >= windowWidth-20){
+                target.xVelocity = Math.abs(target.xVelocity)*-1;
+            }else if (target.offset().left < 0){
+                target.xVelocity = Math.abs(target.xVelocity);
+            }if (target.offset().top >= windowHeight-20){
+                target.yVelocity = Math.abs(target.yVelocity)*-1;
+            }else if (target.offset().top < 0){ 
+                target.yVelocity = Math.abs(target.yVelocity);
+            }
+  
+        }
     }
 
     /**
@@ -128,6 +160,8 @@
             touch.target.movingBox = jThis;
             touch.target.deltaX = touch.pageX - startOffset.left;
             touch.target.deltaY = touch.pageY - startOffset.top;
+            touch.target.movingBox.gravityX = 0;
+            touch.target.movingBox.gravityY = 0;
         });
 
         // Eat up the event so that the drawing area does not
@@ -139,7 +173,12 @@
      * Sets up the given jQuery collection as the drawing area(s).
      */
     var setDrawingArea = function (jQueryElements) {
+    
+        
         // Set up any pre-existing box elements for touch behavior and the bounding box.
+        
+        oldTimestamp = 0;
+        window.requestAnimationFrame(animateBoxes);
  
         windowHeight = (window.innerHeight > 0) ? window.innerHeight-35 : screen.height-35;
         windowWidth = (window.innerWidth > 0) ? window.innerWidth-15 : screen.width-15;    

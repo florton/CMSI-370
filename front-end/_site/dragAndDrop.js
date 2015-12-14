03b9ffdb5dba5041237b35febff40ca48f443b3c
Flanders Lorton <florton@lion.lmu.edu>
2015-12-14 02:07:08 -0800

    made code more portable and work across uses
    still having issues with disabling default dragging and selecting

--- front-end/_site/dragAndDrop.js
+++ front-end/_site/dragAndDrop.js
@@ -6,9 +6,10 @@
     var selectedBox;
 
     var moveObject = function(event) {
+        console.log(dragImage.width);
         dragImage.offset({
-            left: event.pageX - dragImage[0].width / 2,
-            top: event.pageY - dragImage[0].height / 2
+            left: event.pageX - dragImage.width()/2,
+            top: event.pageY - dragImage.height()/2
         });
     };
 
@@ -21,13 +22,13 @@
     var createImage = function(event) {
         selectedBox = $(event.target);
         dragImage = $(event.target).clone();
-        dragImage[0].width = $(event.target)[0].width;
-        dragImage[0].height = $(event.target)[0].height;
-
+        dragImage.width($(event.target).width());
+        dragImage.height($(event.target).height());
+        
         $("body").append(dragImage);
         dragImage.offset({
-            left: event.pageX - dragImage[0].width / 2,
-            top: event.pageY - dragImage[0].height / 2
+            left: event.pageX - dragImage.width() / 2,
+            top: event.pageY - dragImage.height() / 2
         });
         $("body").mousemove(moveObject);
     };
@@ -35,7 +36,6 @@
     var dropImage = function(event) {
         for (var i = 0; i < targets.length; i++) {
             var dropZone = targets[i];
-            console.log(dropZone);
             if (event.pageX > dropZone.offset().left && event.pageX < dropZone.offset().left + dropZone.width()) {
                 if (event.pageY > dropZone.offset().top && event.pageY < dropZone.offset().top + dropZone.height()) {
                     if (selectedBox !== null) {
@@ -51,9 +51,6 @@
     $.fn.dragAndDrop = function() {
         targets = Array.prototype.slice.call(arguments, 0);
         callback = targets.pop();
-
-        selectedBox = $(this);
-
         makeUnselectable($(this));
         this.mousedown(createImage);
         $("body").mouseup(dropImage);
--- widget-from-scratch/dragAndDrop.js
+++ widget-from-scratch/dragAndDrop.js
@@ -7,8 +7,8 @@
 
     var moveObject = function(event) {
         dragImage.offset({
-            left: event.pageX - dragImage[0].width / 2,
-            top: event.pageY - dragImage[0].height / 2
+            left: event.pageX - dragImage.width()/2,
+            top: event.pageY - dragImage.height()/2
         });
     };
 
@@ -21,20 +21,20 @@
     var createImage = function(event) {
         selectedBox = $(event.target);
         dragImage = $(event.target).clone();
-        dragImage[0].width = $(event.target)[0].width;
-        dragImage[0].height = $(event.target)[0].height;
-
+        dragImage.width($(event.target).width());
+        dragImage.height($(event.target).height());
+        
         $("body").append(dragImage);
         dragImage.offset({
-            left: event.pageX - dragImage[0].width / 2,
-            top: event.pageY - dragImage[0].height / 2
+            left: event.pageX - dragImage.width() / 2,
+            top: event.pageY - dragImage.height() / 2
         });
         $("body").mousemove(moveObject);
     };
 
     var dropImage = function(event) {
         for (var i = 0; i < targets.length; i++) {
-            var dropZone = $("#" + targets[i]);
+            var dropZone = targets[i];
             if (event.pageX > dropZone.offset().left && event.pageX < dropZone.offset().left + dropZone.width()) {
                 if (event.pageY > dropZone.offset().top && event.pageY < dropZone.offset().top + dropZone.height()) {
                     if (selectedBox !== null) {
@@ -50,10 +50,7 @@
     $.fn.dragAndDrop = function() {
         targets = Array.prototype.slice.call(arguments, 0);
         callback = targets.pop();
-
-        selectedBox = $(this);
-
-        makeUnselectable($(this));
+        //makeUnselectable($(this));
         this.mousedown(createImage);
         $("body").mouseup(dropImage);
     };
